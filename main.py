93645a6a14122c1e6246b15dd585dd854774fbdb
Asztrik <asztrikpetrovai@gmail.com>
2023-02-01 12:47:58 +0100

    lista kijavítása

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 #megoldás
-def eredmeny(kartyaosszege):
-    jszam = kartyaosszege[1]
-    gszam = kartyaosszege[0]
+def eredmeny(jatekoslapok: [int], geplapok: [int]):
+    jszam: int = lapszamolas(jatekoslapok)
+    gszam: int = lapszamolas(geplapok)
     if jszam > 21:
         print("játékos vesztett")
     if gszam > 21:
         print("gép vesztett")
 
-def lapszamolas(glist, jlist):
-    gszamok=0
-    index = 0
-    while index < len(glist):
-        gszamok += glist[index]
+def lapszamolas(lapok)->int:
+    pontok: int = 0
+    index: int = 0
+    while index < len(lapok):
+        pontok += lapok[index]
         index +=1
+    return pontok
 
-    jszamok=0
-    index = 0
-    while index < len(jlist):
-        jszamok += jlist[index]
-        index +=1
-    kartyakosszege = [gszamok, jszamok]
-    return kartyakosszege
-
-glist = [5, 3, 5, 3, 7, 2]
-jlist = [7, 4, 2, 5, 2]
-lapszamolas(glist, jlist)
-eredmeny(lapszamolas())
 
 #teszteset
 
