5a6e37a1d920be1d9e6e75dea01bfc065ae57c67
Asztrik <asztrikpetrovai@gmail.com>
2023-02-01 12:16:24 +0100

    parameter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #megoldás
-def eredmeny():
-    pass
+def eredmeny(gszam, jszam):
+    if jszam > 21:
+        print("játékos vesztett")
+    if gszam > 21:
+        print("gép vesztett")
 #teszteset
+eredmeny(21, 25)
 
 
